3de21f6cf88f7444bd4451e2111c8b7a5e1a17b2
Mrbysco <Mrbysco@users.noreply.github.com>
2025-03-05 21:35:07 +0100

    Update tooltip of the Player Rug to use a translation key

--- src/main/java/uk/kihira/playerrugs/common/item/PlayerRugItem.java
+++ src/main/java/uk/kihira/playerrugs/common/item/PlayerRugItem.java
@@ -1,24 +1,27 @@
 package uk.kihira.playerrugs.common.item;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.SkullBlockEntity;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-import uk.kihira.playerrugs.PlayerRugs;
 import uk.kihira.playerrugs.client.renderer.PlayerRugInventoryRenderer;
-import uk.kihira.playerrugs.common.blockentity.PlayerRugBlockEntity;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class PlayerRugItem extends BlockItem {
@@ -30,9 +33,15 @@ public class PlayerRugItem extends BlockItem {
     @Override
     public void verifyTagAfterLoad(CompoundTag tag) {
         super.verifyTagAfterLoad(tag);
-        if (tag.contains("PlayerProfile")) {
+        //TagType 8 is a string, 10 is a compound
+        if (tag.contains("PlayerProfile", 8) && !Util.isBlank(tag.getString("PlayerProfile"))) {
+            GameProfile gameprofile = new GameProfile((UUID) null, tag.getString("PlayerProfile"));
+            SkullBlockEntity.updateGameprofile(gameprofile, (profile) -> {
+                tag.put("PlayerProfile", NbtUtils.writeGameProfile(new CompoundTag(), profile));
+            });
+        } else if (tag.contains("PlayerProfile", 10)) {
             GameProfile gameprofile = NbtUtils.readGameProfile(tag.getCompound("PlayerProfile"));
-            PlayerRugBlockEntity.updateGameProfile(gameprofile, (profile) -> {
+            SkullBlockEntity.updateGameprofile(gameprofile, (profile) -> {
                 tag.put("PlayerProfile", NbtUtils.writeGameProfile(new CompoundTag(), profile));
             });
         }
@@ -58,9 +67,22 @@ public class PlayerRugItem extends BlockItem {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, level, tooltip, flag);
-        if (stack.hasTag() && stack.getTag() != null) {
-            final GameProfile profile = NbtUtils.readGameProfile(stack.getTag().getCompound("PlayerProfile"));
-            tooltip.add(Component.literal("Player: " + (profile != null ? profile.getName() : "None")));
+        CompoundTag tag = stack.getTag();
+        MutableComponent playerComponent = Component.translatable("playerrugs.tooltip", Component.literal("None").withStyle(ChatFormatting.RED));
+        if (tag != null) {
+            if (tag.contains("PlayerProfile", 8)) {
+                playerComponent = Component.translatable("playerrugs.tooltip",
+                        Component.literal(tag.getString("PlayerProfile")).withStyle(ChatFormatting.YELLOW));
+            } else {
+                CompoundTag profile = tag.getCompound("PlayerProfile");
+                String name = "None";
+                if (profile.contains("Name", 8)) {
+                    name = profile.getString("Name");
+                }
+                playerComponent = Component.translatable("playerrugs.tooltip",
+                        Component.literal(name).withStyle(ChatFormatting.YELLOW));
+            }
         }
+        tooltip.add(playerComponent);
     }
 }
